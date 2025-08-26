5346534ac8a8410dede44ae099b5753359ebe837
sysantonio <andf92@gmail.com>
2025-08-26 16:47:29 +0200

    feat(reservations): add mock reservation form

--- front/src/app/features/reservations/reservations.module.ts
+++ front/src/app/features/reservations/reservations.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReservationsListComponent } from './reservations-list/reservations-list.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
 
-const routes: Routes = [{ path: '', component: ReservationsListComponent }];
+const routes: Routes = [
+  { path: '', component: ReservationsListComponent },
+  { path: 'new', component: ReservationFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)]
