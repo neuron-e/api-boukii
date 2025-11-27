8cdc0c9f3ee5acd87e4fd6887ff500d10c963fcc
ad17427 <12254712+sysantonio@users.noreply.github.com>
2025-11-27 13:02:16 +0100

    Broadcast v1 fix

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -13,7 +13,7 @@ class BroadcastServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Broadcast::routes([
-            'middleware' => ['auth:api'],
+            'middleware' => ['auth:sanctum'],
         ]);
 
         require base_path('routes/channels.php');
--- config/auth.php
+++ config/auth.php
@@ -40,10 +40,9 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'api' => [
-            'driver' => 'token',
+        'sanctum' => [
+            'driver' => 'sanctum',
             'provider' => 'users',
-            'hash' => false,
         ],
     ],
 
